349e252a4e12f4f74867a6c866f9cabbb5ff4bbe
never-better <71776532+never-better@users.noreply.github.com>
2021-01-11 16:14:35 +0900

    JS: Scroll

--- main.js
+++ main.js
@@ -5,11 +5,25 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', ()=> {
-  console.log(window.scrollY);
-  console.log(navbarHeight);
   if(window.scrollY > navbarHeight){
     navbar.classList.add('navbar--bright');
   } else{
     navbar.classList.remove('navbar--bright');
   }
+})
+
+// Hand Scrolling when tapping on the navbar menu
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbar.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if(link == null){
+    return;
+  }
+
+  console.log(event.target.dataset.link);
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior : 'smooth' });
 })
